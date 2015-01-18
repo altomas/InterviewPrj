5a4cf61ec5e46c5b36777e67b507378c41e8eea9
altomas <altomas@yandex.ru>
2015-01-18 11:27:07 +0200

    Added new one logic and MS tests for words reversing

--- 7_ReverseWords/ReverseWords.cs
+++ 7_ReverseWords/ReverseWords.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace _7_ReverseWords
 {
-  using NUnit.Framework;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Diagnostics;
 
   public class ReverseWords
   {
@@ -17,6 +18,53 @@ namespace _7_ReverseWords
       
       this.sentence = sentence == null ? null : sentence.ToCharArray();
     }
+    enum Direction 
+    {
+        left = 0,
+        right= 2
+    }
+
+    public string GetReversedValue3()
+    {
+        int head = 0;
+        int tail = this.sentence.Length -1;
+
+        while (head < tail)
+        { 
+            var rightcrt = this.MoveAndPullCrt(this.sentence, head, Direction.right); 
+            var leftcrt =  this.MoveAndPullCrt(this.sentence, tail, Direction.left);
+
+            this.sentence[head] = leftcrt;
+            this.sentence[tail] = rightcrt;
+
+            head++;
+            tail--;
+        }
+
+        return new String(this.sentence);
+    }
+
+    private char MoveAndPullCrt(char[] sentence, int start, Direction direction)
+    {
+        int dif = (int)direction - 1;
+
+        var crt = this.sentence[start];
+
+        if ( crt == ' ')
+        {
+            return crt;
+        }
+
+        while (this.sentence[start + dif] != ' ')
+        {
+            crt = this.sentence[start + dif];
+            this.sentence[start + dif] = this.sentence[start];
+        }
+
+        return crt;
+    }
+
+    
 
     public string GetReversedValue2()
     {
@@ -57,7 +105,7 @@ namespace _7_ReverseWords
       return new string(result.ToArray());
     }
 
-    public string GetReversedValue()
+    public string GetReversedValue1()
     {
       if (this.sentence == null)
       {
@@ -104,38 +152,50 @@ namespace _7_ReverseWords
     }
   }
 
-  [TestFixture]
+   [TestClass]
   public class Tests
   {
-    [Test]
-    [TestCase(" My test sentense ", " sentense test My ")]
-    [TestCase("  My test sentense ", " sentense test My  ")]
-    [TestCase(" My test sentense  ", "  sentense test My ")]
-    [TestCase(null, null)]
-    [TestCase("","")]
-    [TestCase(" ", " ")]
+     [TestMethod]
+    //[TestCase(" My test sentense ", " sentense test My ")]
+    //[TestCase("  My test sentense ", " sentense test My  ")]
+    //[TestCase(" My test sentense  ", "  sentense test My ")]
+    //[TestCase(null, null)]
+    //[TestCase("","")]
+    //[TestCase(" ", " ")]
     public void ReverseWordsTest(string underTest, string expected)
     {
-      var undertest = new ReverseWords(underTest);
+        var cases = new[] { 
+             
+             new {arg = " My test sentense ",  res = " sentense test My "},
+              new {arg = "  My test sentense ",  res = " sentense test My  "},
+               new {arg = " My test sentense  ",  res = "  sentense test My "},
+                new {arg = (string)null,  res = (string)null},
+                 new {arg = "",  res = ""},
+                  new {arg = " ",  res = " "},
+         };
 
-      Assert.AreEqual(expected, undertest.GetReversedValue());
+        cases.ToList().ForEach((val) =>
+        {
+            var undertest = new ReverseWords(val.arg);
+
+            RunWithMeasuring(() => { Assert.AreEqual(val.res, undertest.GetReversedValue1()); });
+            RunWithMeasuring(() => { Assert.AreEqual(val.res, undertest.GetReversedValue2()); });
+            RunWithMeasuring(() => { Assert.AreEqual(val.res, undertest.GetReversedValue3()); });
+        });
 
     }
 
-    [Test]
-    [TestCase(" My test sentense ", " sentense test My ")]
-    [TestCase("  My test sentense ", " sentense test My  ")]
-    [TestCase(" My test sentense  ", "  sentense test My ")]
-    [TestCase(null, null)]
-    [TestCase("", "")]
-    [TestCase(" ", " ")]
-    public void ReverseWordsTest2(string underTest, string expected)
+    public static void RunWithMeasuring(Action actionToRun)
     {
-      var undertest = new ReverseWords(underTest);
+        Stopwatch sw = new Stopwatch();
 
-      Assert.AreEqual(expected, undertest.GetReversedValue2());
+        sw.Start();
+        actionToRun();
+        sw.Stop();
 
+        Console.WriteLine(string.Format("{0}: execution time: {1} ticks", actionToRun.Method.Name, sw.ElapsedTicks));
     }
+
   }
 
 }
