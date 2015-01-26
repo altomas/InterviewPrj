8437a446a34b680260d1a9db8157d4e93f448416
altomas <altomas@yandex.ru>
2015-01-26 23:28:06 +0200

    9_CountOnces - comment was added

--- CountOnces/CountOncesLogic.cs
+++ CountOnces/CountOncesLogic.cs
@@ -15,7 +15,13 @@ namespace CountOncesTests
             while (undertest != 0)
             {
                 count++;
+                // I have found a way to turn off the right one bit what gave me a hint
                 undertest = undertest & (undertest - 1);
+                // there are more tricks 
+                // x & (-x) - isolate the right "1" bit
+                // ~x & (x+1) - isolate righ "0" bit
+                // x | (x + 1) - Turn on the right "0" bit.
+                
             }
 
             return count; 
