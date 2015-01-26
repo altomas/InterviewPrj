f863ac454f79fcacae040a45ad63bfca167d279a
altomas <altomas@yandex.ru>
2015-01-26 23:19:39 +0200

    9_CountOnces in nBit value

---
+++ CountOnces/CountOncesLogic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CountOncesTests
+{
+    public class CountOncesLogic
+    {
+        //9. Give a very good method to count the number of ones in a "n" (e.g. 32) bit number.
+        public static int GetOnces(int undertest)
+        {
+            int count = 0;
+
+            while (undertest != 0)
+            {
+                count++;
+                undertest = undertest & (undertest - 1);
+            }
+
+            return count; 
+        }
+    }
+}
---
+++ CountOncesTests/CountOfOncesTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CountOncesTests
+{
+    [TestClass]
+    public class CountOfOncesUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Int32 undertest = 0;
+
+            Assert.AreEqual(0, CountOncesLogic.GetOnces(undertest));
+
+            undertest = 1;
+
+            Assert.AreEqual(1, CountOncesLogic.GetOnces(undertest));
+
+            undertest = 2;
+
+            Assert.AreEqual(1, CountOncesLogic.GetOnces(undertest));
+
+            undertest = 3;
+
+            Assert.AreEqual(2, CountOncesLogic.GetOnces(undertest));
+
+            undertest = 8;
+
+            Assert.AreEqual(1, CountOncesLogic.GetOnces(undertest));
+
+            undertest = 9;
+
+            Assert.AreEqual(2, CountOncesLogic.GetOnces(undertest));
+
+            undertest = -1;
+
+            Assert.AreEqual(32, CountOncesLogic.GetOnces(undertest));
+
+        }
+    }
+}
