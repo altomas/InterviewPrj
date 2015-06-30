fe8789d6256775f6c40442de7ddaff4cf4e8c7c3
altomas <altomas@yandex.ru>
2015-06-30 17:40:02 +0300

    code cleanup

--- UI/AzureDashboard/AzureDashboard/Controllers/BackupDashboardController.cs
+++ UI/AzureDashboard/AzureDashboard/Controllers/BackupDashboardController.cs
@@ -83,18 +83,18 @@ namespace AzureDashboard.Controllers
     #region Public methods
 
     /// <summary>
-    /// The get data.
+    /// The get data as JSON value on UI you can use this controller method.
     /// </summary>
     /// <param name="periodInHours">
     /// The period in hours.
     /// </param>
     /// <returns>
-    /// The <see cref="JsonResult"/>.
+    /// The JsonResult with JSON representation of report data<see cref="JsonResult"/>.
     /// </returns>
     [HttpPost]
     public JsonResult GetReportData(int periodInHours)
     {
-      return this.Json(repository.FormReport(periodInHours));
+      return this.Json(this.repository.FormReport(periodInHours));
     }
 
     /// <summary>
@@ -105,14 +105,13 @@ namespace AzureDashboard.Controllers
     /// </returns>
     public ActionResult Index()
     {
-      AvailabilityData data = repository.FormReport(this.ReportingPeriod);
+      AvailabilityData data = this.repository.FormReport(this.ReportingPeriod);
 
       this.ViewBag.ReportData = new JavaScriptSerializer().Serialize(data);
 
-      AvailabilityData dailyData = repository.FormReport(24 * 7);
+      AvailabilityData dailyData = this.repository.FormReport(24 * 7);
 
       this.ViewBag.DailyReportData = new JavaScriptSerializer().Serialize(dailyData);
-
 
       this.ViewBag.ReportPeriodInfo = string.Format("LAST {0} {1} ", data.Statistics.Length, data.PeriodType == AvailabilityData.PeriodTypeEnum.Day ? "DAYS" : "HOURS");
 
