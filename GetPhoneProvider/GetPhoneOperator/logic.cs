26f7e30e4bf81fd760a30d4ef4e22800dd070a22
oleksandr.zaslon@skelia.com.ua <oleksandr.zaslon@skelia.com.ua>
2015-11-06 11:58:45 +0200

    Phone number detector

---
+++ GetPhoneProvider/GetPhoneOperator/logic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetPhoneOperator
+{
+    public class logic
+    {
+        IDictionary<int, string> operatorsDictionary;
+
+        int shift = 7; // 7 - is phone number digits amount
+
+        public logic(IDictionary<int, string> operatorsDictionary)
+        {
+            this.operatorsDictionary = operatorsDictionary;
+        }
+
+        public string  GetOperatorName(long number)
+        {
+
+            long code = number / (long)Math.Pow(10, shift);
+
+            return operatorsDictionary[(int)code];
+
+        }
+
+    }
+}
---
+++ GetPhoneProvider/PhoneOperatorTest/Tests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+ 
+    using System.Diagnostics;
+    using System.Linq;
+    using GetPhoneOperator;
+using System.Collections.Generic;
+
+    [TestClass]
+    public class ReverseWordsTests
+    {
+        logic logic = new logic(new Dictionary<int, string>() {
+        {8063, "life"},
+        {8098, "kievstar"},
+        {8066, "mts"},
+        {8091, "utel"},
+        {8044, "ground line"}
+        });
+
+        logic2 logic2 = new logic2(new Dictionary<int, string>() {
+        {8063, "life"},
+        {8098, "kievstar"},
+        {8066, "mts"},
+        {8091, "utel"},
+        {8044, "ground line"}
+        });
+
+
+
+        [TestMethod]
+        public void Test()
+        {
+
+            // Initialize
+            var cases = new[] { 
+              new {logic = logic, arg = 80634997252,  res = "life"},
+              new {logic = logic, arg = 80984997252,  res = "kievstar"},
+              new {logic = logic, arg = 80664997252,  res = "mts"},
+              new {logic = logic, arg = 80914997252,  res = "utel"},
+              new {logic = logic, arg = 80444997252,  res = "ground line"},
+                };
+
+
+            cases.ToList().ForEach((val) =>
+            {
+                Assert.AreEqual(val.res, val.logic.GetOperatorName(val.arg));
+            });
+
+        }
+
+        [TestMethod]
+        public void TestLogic2()
+        {
+
+            // Initialize
+            var cases = new[] { 
+              new {logic = logic2, arg = 80634997252,  res = "life"},
+              new {logic = logic2, arg = 80984997252,  res = "kievstar"},
+              new {logic = logic2, arg = 80664997252,  res = "mts"},
+              new {logic = logic2, arg = 80914997252,  res = "utel"},
+              new {logic = logic2, arg = 80444997252,  res = "ground line"},
+                };
+
+
+            cases.ToList().ForEach((val) =>
+            {
+                Assert.AreEqual(val.res, val.logic.GetOperatorName(val.arg));
+            });
+
+        }
+
+        [TestMethod]
+        public void PerformanceTest()
+        {
+            var str = "Test";
+
+
+            for (int i = 0; i < 100000; i++)
+            {
+                str += " Test";
+            }
+
+            RunWithMeasuring(() => { logic.GetOperatorName(80634997252); });
+        }
+
+        public static void RunWithMeasuring(Action actionToRun)
+        {
+            Console.WriteLine();
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            actionToRun();
+            sw.Stop();
+
+            Console.WriteLine(string.Format("{0}: execution time: {1} ticks", actionToRun.Method.Name, sw.ElapsedTicks));
+        }
+    }
+}
